opensearch-project/dashboards-assistant

[Palantir] wrapper component and service
Registry and component to be used by plugins.

Example of usage:
https://github.com/opensearch-project/alerting-dashboards-plugin/pull/852

Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

public/contexts/core_context.tsx
@@ -7,13 +7,12 @@ import {
   OpenSearchDashboardsServices,
   useOpenSearchDashboards,
 } from '../../../../src/plugins/opensearch_dashboards_react/public';
-import { AppPluginStartDependencies, SetupDependencies } from '../types';
-import { SessionLoadService } from '../services/session_load_service';
-import { SessionsService } from '../services/sessions_service';
+import { AssistantPluginStartDependencies, AssistantPluginSetupDependencies } from '../types';
+import { SessionLoadService, SessionsService } from '../services';
 
 export interface AssistantServices extends Required<OpenSearchDashboardsServices> {
-  setupDeps: SetupDependencies;
-  startDeps: AppPluginStartDependencies;
+  setupDeps: AssistantPluginSetupDependencies;
+  startDeps: AssistantPluginStartDependencies;
   sessionLoad: SessionLoadService;
   sessions: SessionsService;
 }

kavilla/dashboards-assistant-1

public/contexts/core_context.tsx
@@ -7,13 +7,12 @@ import {
   OpenSearchDashboardsServices,
   useOpenSearchDashboards,
 } from '../../../../src/plugins/opensearch_dashboards_react/public';
-import { AppPluginStartDependencies, SetupDependencies } from '../types';
-import { ConversationLoadService } from '../services/conversation_load_service';
-import { ConversationsService } from '../services/conversations_service';
+import { AssistantPluginStartDependencies, AssistantPluginSetupDependencies } from '../types';
+import { ConversationLoadService, ConversationsService } from '../services';
 
 export interface AssistantServices extends Required<OpenSearchDashboardsServices> {
-  setupDeps: SetupDependencies;
-  startDeps: AppPluginStartDependencies;
+  setupDeps: AssistantPluginSetupDependencies;
+  startDeps: AssistantPluginStartDependencies;
   conversationLoad: ConversationLoadService;
   conversations: ConversationsService;
 }